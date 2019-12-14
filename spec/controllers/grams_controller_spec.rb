ca0d3ea9c3cf6b12e6cfcb00a79a89a2e2f6a303
Amanda Pippins <apipps8@gmail.com>
2019-12-14 02:25:22 +0000

    connecting the grams to the user update2

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -17,11 +17,8 @@ end
 
   end
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+     user = FactoryBot.create(:user)
+       
       sign_in user
       get :new
       expect(response).to have_http_status(:success)
@@ -35,11 +32,8 @@ end
     expect(response).to redirect_to new_user_session_path
   end
   it "should successfully create a new gram in our database" do
-    user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+    user = FactoryBot.create(:user)
+        
       sign_in user
     post :create, params: { gram: { message: 'Hello!' } }
     expect(response).to redirect_to root_path
@@ -52,11 +46,8 @@ end
 end
 
   it "should properly deal with validation errors" do
-     user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+     user = FactoryBot.create(:user)
+        
       sign_in user
 
   gram_count = Gram.count
