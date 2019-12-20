95b9322a9293c8327a1b69921d8148136670bd5c
Amanda Pippins <apipps8@gmail.com>
2019-12-20 02:36:34 +0000

    Validate the Picture Field on a Gram

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -147,8 +147,14 @@ end
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: { message: 'Hello!' } }
+      post :create, params: {
+    gram: {
+      message: 'Hello!',
+      picture: fixture_file_upload("/pho.jpg", 'image/jpg')
+    }
+  }
       expect(response).to redirect_to root_path
+
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
